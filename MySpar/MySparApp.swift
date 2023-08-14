90f05cfcafc2379cee71acbabc21e79777bef095
Anton Kasian <34170533+AntonKasian@users.noreply.github.com>
2023-08-14 14:13:12 +0400

    Initial commit

---
+++ MySpar/MySparApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySparApp.swift
+//  MySpar
+//
+//  Created by Anton on 14.08.23.
+//
+
+import SwiftUI
+
+@main
+struct MySparApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
